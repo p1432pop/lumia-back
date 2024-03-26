1585e34fbfb7ab1263ce26d62cf942ee9ba09f9a
p1432pop <p1432pop@naver.com>
2024-03-26 19:54:00 +0900

    플레이어 이름 쿠키 추가

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,13 +1,28 @@
-import { Body, Controller, Get, Param, ParseIntPipe, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Req, Res, UsePipes, ValidationPipe } from '@nestjs/common';
 import { PlayerService } from './player.service';
 import { UpdatePlayerDto } from './dto/update-player.dto';
+import { Request, Response } from 'express';
 
 @Controller('player')
 export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
 
   @Get('/:nickname/:season')
-  async get(@Param('nickname') nickname: string, @Param('season', ParseIntPipe) season: number) {
+  async get(
+    @Param('nickname') nickname: string,
+    @Param('season', ParseIntPipe) season: number,
+    @Res({ passthrough: true }) response: Response,
+    @Req() request: Request,
+  ) {
+    //최근 검색한 플레이어의 이름을 쿠키로 저장
+    if (request.cookies.nicknames) {
+      const set = new Set(request.cookies.nicknames);
+      set.add(nickname);
+      const arr = Array.from(set);
+      response.cookie('nicknames', arr);
+    } else {
+      response.cookie('nicknames', [nickname]);
+    }
     return await this.playerService.get(nickname, season);
   }
   @Post()
